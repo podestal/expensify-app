52ae0d610dc2482998a28e533465554215df2785
podestal <luisrodriguezpodesta@gmail.com>
2024-03-08 11:27:08 -0500

    navigators done

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import ManageExpenses from './screens/ManageExpenses';
+import RecentExpenses from './screens/RecentExpenses';
+import AllExpenses from './screens/AllExpenses';
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator()
+  const BottomTabs = createBottomTabNavigator()
+
+  const ExpensesOverview = () => {
+    return (
+      <BottomTabs.Navigator>
+        <BottomTabs.Screen name='RecentExpenses' component={RecentExpenses}/>
+        <BottomTabs.Screen name='AllExpenses' component={AllExpenses}/>
+      </BottomTabs.Navigator>
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hola Mundo</Text>
-      <StatusBar style="auto" />
-    </View>
+      <>
+        <StatusBar style="auto" />
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name='ExpensesOverview' component={ExpensesOverview}/>
+            <Stack.Screen name='ManageExpense' component={ManageExpenses}/>
+          </Stack.Navigator>
+        </NavigationContainer>
+      </>
   );
 }
 
