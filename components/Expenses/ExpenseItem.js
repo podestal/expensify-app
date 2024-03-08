b678920aee0b66b96f56481092032f60debbf011
podestal <luisrodriguezpodesta@gmail.com>
2024-03-08 13:07:28 -0500

    date formatter utility

--- components/Expenses/ExpenseItem.js
+++ components/Expenses/ExpenseItem.js
@@ -1,4 +1,5 @@
 import { Pressable,View, Text, StyleSheet } from "react-native"
+import { getFormattedDate } from "../../utils/date"
 
 const ExpenseItem = ({ expenses}) => {
 
@@ -7,7 +8,7 @@ const ExpenseItem = ({ expenses}) => {
         <View style={styles.container}>  
             <View>  
                 <Text style={styles.description}>{expenses.description}</Text>
-                <Text>On: {expenses.date.toString()}</Text>
+                <Text>On: {getFormattedDate(expenses.date)}</Text>
             </View>
             <View>
                 <Text style={styles.amount}>{expenses.amount}</Text>
@@ -25,6 +26,9 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         alignItems: 'center',
         marginVertical: 25,
+        borderBottomColor: '#333',
+        borderBottomWidth: 1,
+        paddingBottom: 24,
     },
     description: {
         fontSize: 18,
