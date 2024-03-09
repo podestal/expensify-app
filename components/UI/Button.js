065e30069f86aebeba55bad30ab88bdb9b29fab4
podestal <luisrodriguezpodesta@gmail.com>
2024-03-09 05:55:42 -0500

    adding buttons to manage expense

---
+++ components/UI/Button.js
@@ -0,0 +1,33 @@
+import { View , Pressable, Text, StyleSheet} from "react-native"
+
+const Button = ({ children, onPress, mode, style}) => {
+
+  return (
+    <View style={style}>
+        <Pressable onPress={onPress}>
+            <View style={styles.button}>
+                <Text style={styles.buttonText}>{children}</Text>
+            </View>
+        </Pressable>
+    </View>
+  )
+}
+
+export default Button
+
+const styles = StyleSheet.create({
+    button: {
+        borderRadius: 10,
+        padding: 10,
+    },
+    flat: {
+        backgroundColor: 'transparent'
+    },
+    buttonText: {
+        textAlign: 'center'
+    },
+    pressed: {
+        opacity: 0.75,
+        borderRadius: 4,
+    }
+})
--- screens/ManageExpenses.js
+++ screens/ManageExpenses.js
@@ -1,5 +1,7 @@
-import { View, Text } from "react-native"
+import { View, StyleSheet } from "react-native"
 import { useLayoutEffect } from "react"
+import IconButton from "../components/UI/IconButton"
+import Button from "../components/UI/Button"
 
 const ManageExpenses = ({ route, navigation }) => {
 
@@ -12,11 +14,52 @@ const ManageExpenses = ({ route, navigation }) => {
         })
     })
 
+    const deleteExpenseHandler = () => {
+
+    }
+
+    const cancelHandler = () => {
+
+    }
+
+    const confirmHandler = () => {
+
+    }
+
   return (
-    <View>
-        <Text>Manage Expenses</Text>
+    <View style={styles.container}>
+        <View style={styles.buttons}>
+            <Button style={styles.button} onPress={cancelHandler}>Cancel</Button>
+            <Button style={styles.button} onPress={confirmHandler}>{isEditing ? 'Update' : 'Add'}</Button>
+        </View>
+        {isEditing && (
+            <View style={styles.deleteContainer}>
+                <IconButton icon='trash' size={30} onPress={deleteExpenseHandler}/>
+            </View>
+        )}
     </View>
   )
 }
 
 export default ManageExpenses
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 24,
+    },
+    deleteContainer: {
+        marginTop: 15,
+        paddingTop: 10,
+        alignItems: 'center'
+    },
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    button: {
+        minWidth: 120,
+        marginHorizontal: 8,
+    }
+})
